6fbb1983c71380cf70c698dce1e4d6a1cb7d8a8a
Kevin Midkiff <midkiff_k@students.lynchburg.edu>
2014-04-01 12:45:07 -0400

    updated scripts

---
+++ MoveToTargetTest.cs
@@ -0,0 +1,58 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MoveToTargetTest : MonoBehaviour {
+	
+	public float speed = 4f;
+	public Transform wayPoint;
+	public Transform[] wayPointArray;
+	private int wayPointIndex_=0;
+	// Use this for initialization
+	void Start () 
+	{
+		wayPoint = wayPointArray [wayPointIndex_];
+	}
+
+	// Update is called once per frame
+	void Update () {
+		if (wayPointIndex_ < wayPointArray.Length) 
+		{
+			ChangePosition ();
+		}
+		else
+		{	
+			Destroy(gameObject);
+		}
+		//TEST
+		Assert (wayPointIndex_ <= wayPointArray.Length);
+		Assert (rigidbody.velocity.x == speed || rigidbody.velocity.y == speed);
+	}
+	void ChangePosition()
+	{
+		if (wayPoint) 
+		{
+			transform.position = Vector3.MoveTowards (transform.position, wayPoint.transform.position, speed * Time.deltaTime);
+		}
+	}
+	void OnTriggerEnter (Collider collider)
+	{	
+		if (collider.gameObject.tag == "Turn") {
+			Debug.Log("EnterTurn");
+			wayPointIndex_++;  
+			wayPoint = wayPointArray [wayPointIndex_];
+			}
+		if (collider.gameObject.tag == "Bullet") {
+			Debug.Log("Entered!Bullet");
+			Destroy(gameObject);	
+		}
+
+	}
+	void Assert(bool condition)
+	{
+		if(!condition)
+		{
+			Debug.LogError("Error");
+		}
+		
+	}
+}
---
+++ Turret.cs
@@ -0,0 +1,51 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Turret : MonoBehaviour {
+	public Transform Target;
+	public float damp = 6.0f;
+	public Transform bulletPrefab;
+	public float nextShotTime = 0.0f;
+	public float timeBetweenShots = 2.0f;
+	public float MaxRange = 5.0f;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+
+			Target= GameObject.FindGameObjectWithTag("Balloon").transform;
+			float Distance = Vector3.Distance(Target.position, transform.position);
+			Debug.Log(Distance);
+						if (Distance < MaxRange) {
+								transform.LookAt (Target.position);	
+								if (nextShotTime <= Time.time) {
+										Debug.Log ("SHOOOT");
+										Shoot ();
+										nextShotTime = Time.time + timeBetweenShots;
+								}
+						}
+				
+	}
+	void Shoot()
+	{	
+		Vector3 bulletPosition= new Vector3(bulletPrefab.position.x, bulletPrefab.position.y, -1); 
+		Transform bullet = (Transform)Instantiate(bulletPrefab, bulletPosition, transform.rotation);
+		//bullet.rotation = transform.rotation;
+		bullet.rigidbody.AddForce(transform.forward * 1000);
+	}
+
+
+		void Assert(bool condition)
+	{
+		if(!condition)
+		{
+			Debug.LogError("Error");
+		}
+
+	}
+}
